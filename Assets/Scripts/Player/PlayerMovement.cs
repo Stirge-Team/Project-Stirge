c69ae8482b4e231a5302538d46dbd67f767c7ca1
Jackson Lucas <jacksonjlucas04@gmail.com>
2026-02-24 16:04:22 +1030

    Fall Speed, Jump Height, Input Scaling & Fiction
    * Changed the state settings to be 2 unique variables rather then 2
    variables in an array.
    * Added delta time to the movement input
    * Streamlined the speed limiting
    * Comment cleanup
    - Removed box collider on test players eyes
    + Added a new "fiction" variable to the movement state struct, allowing
    for more refined control over the player's decceleration
    + Added a new player material as fiction/decceleration is not being
    handled by that system anymore
    + Added a strength scale for the player input, so now the input that the
    player gives can be weaker or stronger at different points
    + Player now uses a jump height rather then a force which should remain
    consistant no matter how strong the gravity is
    + Added Fall Speed. Now after the player has been falling for some time,
    they can now begin to fall faster

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,38 +8,55 @@ public class PlayerMovement : MonoBehaviour
   [System.Serializable]
   private struct stateVariables
   {
-    [SerializeField]
-    private string m_name;
-    [Tooltip("The acceleriation of the player")]
+    [Tooltip("The acceleriation of the player (deltaTime involved). DO NOT see this value far greater then the maximum speed, this will cause the player to jump around.")]
     public float _horizontalAcceleration;
     [Tooltip("The maximum velocity the player can reach")]
     public float _maximumHorizontalSpeed;
     [Tooltip("The speed at which the player turns around to face the given input direction")]
     public float _rotationSpeed;
+    [Tooltip("The amount of fiction/decceleration that is applied in this state (deltaTime involved)")]
+    public float _fiction;
+    [Tooltip("The strength the player's input has on the force applied")]
+    public AnimationCurve _inputStrength;
   }
+  //The input from the player
 	private Vector2 m_inputDirection = new Vector2();
+  //I bet you can't guess what this one is for
 	private Rigidbody m_playerBody;
-	[Header("Horizontal Movement")]
-  [SerializeField, Tooltip("These are the values that will be used while the player is 1st: Grounded, 2nd: Aerial")]
-  private stateVariables[] m_StateSettings = new stateVariables[2];
-  private stateVariables m_currentStateSettings => m_StateSettings[Convert.ToInt32(!IsGrounded)];
+	[Header("Horizontal Movement Settings")]
+  [SerializeField, Tooltip("These are the values that will be used while the player is grounded")]
+  private stateVariables m_groundSettings;
+  [SerializeField, Tooltip("These are the values that will be used while the player is in the air.")]
+  private stateVariables m_aerialSettings;
+  //Selector for the settings
+  private stateVariables m_currentStateSettings => IsGrounded ? m_groundSettings : m_aerialSettings;
 
-	[Header("Vertical Movement")]
-	[SerializeField, Tooltip("The amount of force used to push the player object up.")]
-	private float m_jumpForce;
+	[Header("Jump Settings")]
+	[SerializeField, Tooltip("The desired height you'd like the player to reach.")]
+	private float m_jumpHeight = 5f;
+  //Grounded bool
 	public bool IsGrounded {get; private set;}
+  [SerializeField, Tooltip("The distance from the center of the player that considers them grounded.")]
+  private float m_groundCheckDistance;
+  //The layers that the player considers "ground"
+  private LayerMask m_groundCheckLayers;
 	[SerializeField, Tooltip("The window after falling that the player can still jump.")]
-	private float m_coyoteTime;
+	private float m_coyoteTime = 0.2f;
+  //The remaining time for coyote time
 	private float m_coyoteCountdown;
-	[SerializeField, Tooltip("The distance from the center of the player that considers them grounded.")]
-	private float m_groundCheckDistance;
-	private LayerMask m_groundCheckLayers;
-  [SerializeField, Tooltip("The maximum speed the player can fall (0 will skip this check0"), Min(0)]
+  [Header("Fall Speed")]
+  [SerializeField, Tooltip("The maximum speed the player can fall (0 will skip this check"), Min(0)]
   private float m_fallSpeedCap = 0;
-
+  [SerializeField, Tooltip("How much the time the player has spent falling should affect their falling speed.")]
+  private float m_fallTimeSpeedMultiplier = 0;
+  //The time the player has been falling
+  private float m_currentFallTime = 0;
+  //The maximum height the player reached - used for the fall speed mult check
+  private float m_lastCheckedHeight = 0;
 
 	private Transform m_cameraTransform;
-	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	
+
 	void Start()
 	{
 		//Gets all relivant components
@@ -54,41 +71,38 @@ public class PlayerMovement : MonoBehaviour
 	{
     //Calculates the direction to move the player in given the current inputs and camera transform
     Vector3 attemptedMoveDirection = (new Vector3(m_cameraTransform.forward.x, 0, m_cameraTransform.forward.z) * m_inputDirection.y + new Vector3(m_cameraTransform.right.x, 0, m_cameraTransform.right.z) * m_inputDirection.x).normalized;
-    //Rotates the player object to face the given direction
     //Only when the player applies any directional inputs...
     if(attemptedMoveDirection.magnitude > 0)
     {
       //Interperlate the rotations between the current player rotation and the given input direction
       transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(attemptedMoveDirection), m_currentStateSettings._rotationSpeed);
     }
-    //Quaternion rotationDirDelta = transform.rotation * lookDir;
-    //transform.rotation = Quaternion.Lerp(transform.rotation, lookDir, rotationDirDelta.magnitude);
-    //transform.LookAt(transform.position + attemptedMoveDirection); //change to a lerp
-    //Resets the rotation so the player object isn't facing down
-    transform.Rotate(-transform.rotation.x, 0, 0);
-    //Setup the force that will be applied
-    Vector3 moveForce = m_inputDirection.magnitude * transform.forward * m_currentStateSettings._horizontalAcceleration;
 
-    //Finds the player's current horizontal velocity
+    //Stop the player from looking at the ground
+    transform.Rotate(-transform.rotation.x, 0, 0);
+
+    //Get the player's horizontal velocity
     Vector3 playerBodyHorizontalVelocity = new Vector3 (m_playerBody.linearVelocity.x, 0, m_playerBody.linearVelocity.z);
-    //Estimate what the resulting velocity will be - IDK how acurate this is but it works so don't touch it
-    Vector3 estimVelocityDelta = (playerBodyHorizontalVelocity + moveForce * Time.deltaTime);
-    if(estimVelocityDelta.magnitude < m_currentStateSettings._maximumHorizontalSpeed)
+
+    //If the player's current horizontal velocity is less then the speed limit, then the player can be moved
+    if(playerBodyHorizontalVelocity.magnitude < m_currentStateSettings._maximumHorizontalSpeed)
     {
-      //Applies the calculated movement to the players current position.
-      m_playerBody.AddForce(moveForce);
+      //Apply the force to the player
+      m_playerBody.AddForce(m_currentStateSettings._inputStrength.Evaluate(m_inputDirection.magnitude) * m_inputDirection.magnitude * transform.forward * m_currentStateSettings._horizontalAcceleration * Time.deltaTime);
     }
-		
+
+    //do some decceleration - the clamped value helps when getting the movement down to zero
+    m_playerBody.AddForce(playerBodyHorizontalVelocity.normalized * -m_currentStateSettings._fiction * Mathf.Clamp(playerBodyHorizontalVelocity.magnitude, 0, 1) * Time.deltaTime);
+    
+    //Clamping the players fall speed
     if(m_fallSpeedCap > 0 && -m_fallSpeedCap > m_playerBody.linearVelocity.y)
     {
-      //Debug.Log($"Player fall speed: {m_playerBody.linearVelocity.y}");
       m_playerBody.linearVelocity -= new Vector3(0, m_playerBody.linearVelocity.y + m_fallSpeedCap, 0); 
     }
 		
-    //Casts a ray down from the player's center, and outs true if a ground layer object is hit
+    //Casts a sphere down from the player's center, and outs true if a ground layer object is hit
 		if(Physics.CheckSphere(transform.position + Vector3.down * m_groundCheckDistance, 0.5f, m_groundCheckLayers))
 		{
-			//Debug.DrawSphere(transform.position + Vector3.down * m_groundCheckDistance, 1, Color.green);
 			//Check if the player is not considered grounded
 			if(!IsGrounded)
 			{
@@ -96,11 +110,12 @@ public class PlayerMovement : MonoBehaviour
 				IsGrounded = true;
 				//Reset the coyote time
 				m_coyoteCountdown = m_coyoteTime;
+        //Reset the fall time
+        m_currentFallTime = 0;
 			}
 		}
 		else
 		{
-			Debug.DrawRay(transform.position, Vector3.down * m_groundCheckDistance, Color.red);
 			//Check if there is remaining coyote time
 			if(m_coyoteCountdown > 0 && IsGrounded)
 			{
@@ -112,7 +127,27 @@ public class PlayerMovement : MonoBehaviour
 			{
 				//The player is no longer grounded
 				IsGrounded = false;
+        //Reset the last checked height
+        m_lastCheckedHeight = transform.position.y;
 			}
+
+      if(!IsGrounded)
+      {
+        //First determine if the player is falling
+        if(m_lastCheckedHeight > transform.position.y)
+        {
+          //Start timing the player's fall
+          m_currentFallTime += Time.deltaTime;
+        
+          //Add a little more force to the player when they have been falling for a while.
+          m_playerBody.AddForce(-transform.up * m_currentFallTime * m_fallTimeSpeedMultiplier);
+        }
+        //Update the last height if the player is height than before
+        else if(m_lastCheckedHeight < transform.position.y)
+        {
+          m_lastCheckedHeight = transform.position.y;
+        }
+      }
 		}
 	}
 
@@ -128,7 +163,7 @@ public class PlayerMovement : MonoBehaviour
 		if (IsGrounded)
 		{
 			//Apply a force up on the player
-			m_playerBody.AddForce(transform.up * m_jumpForce);
+			m_playerBody.AddForce(transform.up * Mathf.Sqrt(2 * m_jumpHeight * -Physics.gravity.y), ForceMode.Impulse);
 			//Remove all coyote time 
 			m_coyoteCountdown = 0;
 			//Grounded is not set to off here as the first check in fixed update will reset the player to being grounded in this frame
