b84e226cf708d810beca130104dfd18552513288
Jackson Lucas <jacksonjlucas04@gmail.com>
2026-05-18 19:46:33 +0930

    Fixed sliding player issue.
    Issue caused by a lack of friction as the call to apply friction was
    being ignored by a disabled movement motor while the player was stunned.
    Also added a flip time to Halt all horizontal velocity function in the
    movement motor for convinence.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -190,12 +190,7 @@ namespace Stirge.Player
             }
 
             //do some decceleration - the clamped value helps when getting the movement down to zero
-            m_motor.ApplyForce(
-                m_motor._horizontalDirection
-                    * -m_currentStateSettings._friction
-                    * Mathf.Clamp(m_motor._horizontalSpeed, 0, 1)
-                    * Time.deltaTime
-            );
+            m_motor.ApplyForce(m_motor._horizontalDirection * -m_currentStateSettings._friction * Mathf.Clamp(m_motor._horizontalSpeed, 0, 1) * Time.deltaTime, ForceMode.Force, true);
 
             //Clamping the players fall speed
             m_motor.ClampVerticalVelocity(-m_fallSpeedCap);
