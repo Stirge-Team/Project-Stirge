b57301d763cb27743808f5b3bd2f0fb5e9003530
Jackson Lucas <jacksonjlucas04@gmail.com>
2026-06-09 14:25:30 +0930

    Indexed searching
    \* Updated all the remaining animations with particle effect on hit.
    \* Updated player prefab to better display particles on the launcher
    attack.
    \* Default attack particle now simulated in world space.
    \* Cleaned up the particle class Play Particle function.
    \* Particles can now be referenced by index as well as name!

--- Assets/Scripts/Visuals/ParticleInstancer.cs
+++ Assets/Scripts/Visuals/ParticleInstancer.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEditor.Search;
 using UnityEngine;
 
 public class ParticleInstancer : MonoBehaviour
@@ -13,15 +14,11 @@ public class ParticleInstancer : MonoBehaviour
         private ParticleSystem m_particleEffect;
         [SerializeField]
         private Transform m_defaultLocation;
-        public void PlayParticle(Transform location)
+        public void PlayParticle(Transform location = null)
         {
             ParticleSystem newInst = Instantiate(m_particleEffect);
-            newInst.transform.SetParent(location, false);
+            newInst.transform.SetParent(location != null ? location : m_defaultLocation, false);
             newInst.Play();
-        }
-        public void PlayParticle()
-        {
-            PlayParticle(m_defaultLocation);
         }
     }
     [SerializeField]
@@ -29,16 +26,27 @@ public class ParticleInstancer : MonoBehaviour
 
     public void PlayParticle(string effectName, Transform location = null)
     {
-        foreach (var effect in m_particles)
+        int index = -1;
+        try
         {
-            if (effect.Name == effectName)
+            index = Convert.ToInt32(effectName);
+            if (index >= m_particles.Length)
+                throw new IndexOutOfRangeException("The given index is outside of the particle instancer array!");
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Given string is NOT a valid int. Continuing as normal...\n" + e);
+        }
+
+        for (int i = index >= 0 ? index : 0; i < m_particles.Length; i++)
+        {
+            if (m_particles[i].Name == effectName || index == i)
             {
-                if (!location)
-                    effect.PlayParticle();
-                else
-                    effect.PlayParticle(location);
+                m_particles[i].PlayParticle(location);
+                return;
             }
         }
+        Debug.LogError($"No particle found with name {effectName}. Please make sure you're calling the correct paricle instancer and that your strings are matching.");
     }
 
     //this function is here to faciliate unityevents being very very limited in the parameters they pass which is stupid and dumb, let me pass 30000 params if that is what i please.
