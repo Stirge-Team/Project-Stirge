28293508a97112e37a69d1bc1e893914c470f4ef
Jackson Lucas <jacksonjlucas04@gmail.com>
2026-06-16 16:30:48 +0930

    Made the system simpler.
    Realised that the system was overcomplicated, made some adjustements to
    remedy this.
    \* Changes to the Destructable object script and demo scene

--- Assets/Scripts/Environmental/DestructableObject.cs
+++ Assets/Scripts/Environmental/DestructableObject.cs
@@ -4,13 +4,19 @@ namespace Stirge
 {
     public class DestructableObject : MonoBehaviour
     {
-        public GameObject m_brokenStateObject;
+        //public GameObject m_brokenStateObject;
         public float m_forceThreashhold;
         public float m_impactExaggeration = 1;
         public ParticleSystem m_dustParticles;
-
-        public void OnCollisionEnter(Collision collision)
+        void Start()
         {
+            SetChildKinematics(true);
+        }
+
+        public void OnTriggerEnter(Collider collision)
+        {
+            if(collision.transform.IsChildOf(transform)) return; //exit if child object
+
 
             Rigidbody rigidbody = collision.gameObject.GetComponent<Rigidbody>(); //get the incoming rigidbody
 
@@ -22,25 +28,9 @@ namespace Stirge
             {
                 if (rigidbody.linearVelocity.sqrMagnitude >= m_forceThreashhold) //are they moving fast enough to break this object?
                 {
+                    var direction = -(rigidbody.transform.position - transform.position).normalized;
                     Debug.DrawRay(rigidbody.transform.position, rigidbody.linearVelocity, Color.red, 3);
-                    if (m_brokenStateObject)
-                    {
-                        GameObject debris = Instantiate(m_brokenStateObject, transform.position, Quaternion.identity); //spawn debris
-                        debris.transform.localScale = transform.localScale;
-
-                        //apply force to it and any child objects!
-                        Rigidbody debrisRB = debris.GetComponent<Rigidbody>();
-                        if (debrisRB)
-                        {
-                            debrisRB.AddForceAtPosition(-rigidbody.linearVelocity * m_impactExaggeration, collision.transform.position, ForceMode.Impulse);
-                            Debug.DrawRay(debris.transform.position, -rigidbody.linearVelocity * m_impactExaggeration, Color.blue, 3);
-                        }
-                        foreach (var childRB in debris.GetComponentsInChildren<Rigidbody>())
-                        {
-                            childRB.AddForceAtPosition(-rigidbody.linearVelocity * m_impactExaggeration, collision.transform.position, ForceMode.Impulse);
-                            Debug.DrawRay(childRB.transform.position, -rigidbody.linearVelocity * m_impactExaggeration, Color.blue, 3);
-                        }
-                    }
+                    SetChildKinematics(direction * rigidbody.linearVelocity.sqrMagnitude * m_impactExaggeration, rigidbody.transform.position);
 
                     if (m_dustParticles)
                     {
@@ -50,8 +40,24 @@ namespace Stirge
                         particleInstance.Play();
                     }
 
-                    Destroy(gameObject);
+                    gameObject.GetComponent<Collider>().enabled = false; //disable collider
                 }
+            }
+        }
+
+        private void SetChildKinematics(bool setValue)
+        {
+            foreach (var child in GetComponentsInChildren<Rigidbody>())
+            {
+                child.isKinematic = setValue;
+            }
+        }
+        private void SetChildKinematics(Vector3 force, Vector3 position)
+        {
+            foreach(var child in GetComponentsInChildren<Rigidbody>())
+            {
+                child.isKinematic = false;
+                child.AddForceAtPosition(force, position, ForceMode.Impulse);
             }
         }
     }
