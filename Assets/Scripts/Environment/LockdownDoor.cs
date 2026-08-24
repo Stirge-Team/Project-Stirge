165db0e882cc27dd624500dd4f98773179e28bd7
Jackson Lucas <jacksonjlucas04@gmail.com>
2026-08-24 13:45:47 +0930

    Basic lockdown door added.
    + Added basic lockdown door object (animated + scripted).

---
+++ Assets/Scripts/Environment/LockdownDoor.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+namespace Stirge.Environment
+{
+    public class LockdownDoor : MonoBehaviour
+    {
+        private Animator m_mdlAnim;
+        [SerializeField]
+        private bool m_locked;
+        private Collider m_col;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            m_col = GetComponent<Collider>();
+            m_mdlAnim = GetComponentInChildren<Animator>();
+
+            ToggleLockedDoor(m_locked ? 1 : 0);
+        }
+
+        /// <summary>
+        /// Changes the lock state of this door.
+        /// </summary>
+        /// <param name="forceState">Sets the lock state rather than fliping it. 0 == Unlock, 1 == Lock</param>
+        /// <returns></returns>
+        public bool ToggleLockedDoor(int forceState = -1)
+        {
+            
+            m_locked = forceState switch
+            {
+                0 => false,
+                1 => true,
+                _ => !m_locked,
+            };
+
+            m_col.enabled = m_locked ? true : false;
+            m_mdlAnim.SetTrigger(m_locked ? "Lock" : "Unlock");
+            return m_locked;
+        }
+        public void UnlockDoor()
+        {
+            ToggleLockedDoor(0);
+        }
+        public void LockDoor()
+        {
+            ToggleLockedDoor(1);
+        }
+    }
+}
