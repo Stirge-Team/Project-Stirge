18f3f43f8f347cc94af18dedb7c073b759ca25ba
Jackson Lucas <jacksonjlucas04@gmail.com>
2026-08-26 17:23:04 +0930

    Wave class update.
    * Many of the Wave Spawner vars moved into the "Wave" class. This should
    help with making custom waves more... custom. TL;DR I moved a bunch of
    lines up ~50 lines and fixed the errors.

--- Assets/Scripts/Combat/WaveSpawner.cs
+++ Assets/Scripts/Combat/WaveSpawner.cs
@@ -10,14 +10,14 @@ namespace Stirge.Combat
     public class WaveSpawner : MonoBehaviour
     {
         [System.Serializable]
-        public struct WaveEnemyData
+        public class WaveEnemyData
         {
             [SerializeField]
             private Enemy m_type;
             [SerializeField]
-            private int m_maxCount;
+            private int m_maxCount = 1;
             private int m_spawnedCount;
-            public bool canSpawn => m_spawnedCount < m_maxCount;
+            public bool _spawnsAvalible => m_spawnedCount < m_maxCount;
             public Enemy SpawnEnemy(Vector3 pos)
             {
                 Enemy newEnemy = Instantiate(m_type, pos, Quaternion.identity);
@@ -27,29 +27,63 @@ namespace Stirge.Combat
             }
         }
         [System.Serializable]
-        public struct Wave
+        public class Wave
         {
             [SerializeField] //x,y are the center point, z,w are the bounds. i see *no* issue with this
             private Vector4 m_spawnArea;
-            public Vector4 SpawnArea { readonly get { return m_spawnArea; } set { } }
+            public Vector4 SpawnArea { get { return m_spawnArea; } set { } }
+            [SerializeField]
+            private float m_startDelay = 1f;
+            private bool m_delayComplete => m_startDelay <= 0;
+            [SerializeField]
+            private float m_spawnRate = 0.5f;
+            private float m_spawnRateCountdown;
+            private bool m_spawnCountdownComplete => m_spawnRateCountdown <= 0;
+            [SerializeField, Tooltip("How many enemies (within the maximum) should be spawned at once.")]
+            private int m_batchSpawn = 1;
+            [SerializeField, Tooltip("Can the number of enemies exceed the maximum limit if spawning a batch of enemies would cause it to do so. i.e. enemy limit is 4, spawning in batches of 3. First batch spawns in, then the second resulting in 6 enemies rather then 4.")]
+            private bool m_canBatchOverflow = false;
+            [SerializeField]
+            private int m_maxSpawnCount = 3;
             [SerializeField, Tooltip("The list of enemies to spawn. They will be spawned in the order listed.")]
-            private WaveEnemyData[] m_enemies;
-            public Enemy AttemptSpawnEnemy(Vector3 origin)
+            private WaveEnemyData[] m_enemyList;
+            private int m_spawnIndex;
+            public bool _outOfSpawns => m_spawnIndex >= m_enemyList.Count();
+            public void Init()
             {
-                float rndXBound = Random.Range(-m_spawnArea.z / 2, m_spawnArea.z / 2);
-                float rndZBound = Random.Range(-m_spawnArea.w / 2, m_spawnArea.w / 2);
-                Vector3 position = origin + new Vector3(m_spawnArea.x, 0, m_spawnArea.y) + new Vector3(rndXBound, 0, rndZBound); //origin + set offset + rnd offset within range
-
-                //TODO: Add player avoidance spawn code so that enemies don't spawn on the player
-
-                for (int i = 0; i < m_enemies.Length; i++)
+                m_spawnRateCountdown = m_spawnRate;
+                m_spawnIndex = 0;
+                if (m_batchSpawn > m_maxSpawnCount && m_canBatchOverflow) Debug.LogWarning("Enemy batch spawn count exceeds the maximum enemy count. Please keep the Batch Spawn value less then or equal to the max limit unless this was intened.");
+            }
+            public Enemy[] AttemptSpawnEnemy(Vector3 origin, WaveSpawner spawner)
+            {
+                if (m_delayComplete && m_spawnCountdownComplete && !_outOfSpawns && spawner._activeEnemies.Count < m_maxSpawnCount) //check countdowns
                 {
-                    if (m_enemies[i].canSpawn)
+                    Enemy[] batch = new Enemy[m_batchSpawn];
+                    for (int x = 0; x < m_batchSpawn; x++) //spawn the batch amount
                     {
-                        return m_enemies[i].SpawnEnemy(position);
+                        if (m_enemyList[m_spawnIndex]._spawnsAvalible) //check for any avalible spawns
+                        {
+                            //spawning position code
+                            float rndXBound = Random.Range(-m_spawnArea.z / 2, m_spawnArea.z / 2);
+                            float rndZBound = Random.Range(-m_spawnArea.w / 2, m_spawnArea.w / 2);
+                            Vector3 position = origin + new Vector3(m_spawnArea.x, 0, m_spawnArea.y) + new Vector3(rndXBound, 0, rndZBound); //origin + set offset + rnd offset within range
+
+                            //TODO: Add player avoidance spawn code so that enemies don't spawn on the player
+
+                            batch[x] = m_enemyList[m_spawnIndex].SpawnEnemy(position); //instance the enemy and save
+                            m_spawnRateCountdown = m_spawnRate; //reset countdown
+                            if (!m_enemyList[m_spawnIndex]._spawnsAvalible) m_spawnIndex++; //increase the spawn index if all the avalible enemies of that type have spawned.
+                        }
                     }
+                    return batch;
                 }
-                return null;
+
+                if (!m_delayComplete) m_startDelay -= Time.deltaTime;
+                if (!m_spawnCountdownComplete) m_spawnRateCountdown -= Time.deltaTime;
+
+                Enemy[] mt = new Enemy[0];
+                return mt;
             }
         }
 
@@ -61,19 +95,8 @@ namespace Stirge.Combat
         [SerializeField]
         private UnityEvent m_completeEvent;
 
-        [SerializeField]
-        private float m_waveDelay = 2f;
-        private float m_waveDelayCountdown = 0f;
-        [SerializeField]
-        private float m_spawnRate = 1f;
-        private float m_spawnRateCountdown = 0f;
-        [SerializeField, Tooltip("How many enemies (within the maximum) should be spawned at once.")]
-        private int m_batchSpawn = 1;
-        [SerializeField, Tooltip("Can the number of enemies exceed the maximum limit if spawning a batch of enemies would cause it to do so. i.e. enemy limit is 4, spawning in batches of 3. First batch spawns in, then the second resulting in 6 enemies rather then 4.")]
-        private bool m_canBatchOverflow = false;
         private List<Enemy> m_activeEnemies = new();
-        [SerializeField]
-        private int m_maxSpawnCount = 5;
+        public List<Enemy> _activeEnemies { get { return m_activeEnemies; } }
         private ParticleInstancer m_particles;
         [SerializeField]
         private string m_spawnParticleName;
@@ -91,9 +114,8 @@ namespace Stirge.Combat
             enabled = true;
             m_particles = GetComponent<ParticleInstancer>();
             m_activeEnemies = new();
-            m_waveDelayCountdown = m_waveDelay;
-            m_spawnRateCountdown = m_spawnRate;
-            if (m_batchSpawn > m_maxSpawnCount && m_canBatchOverflow) Debug.LogWarning("Enemy batch spawn count exceeds the maximum enemy count. Please keep the Batch Spawn value less then or equal to the max limit unless this was intened.");
+            m_waveIndex = 0;
+            m_currentWave.Init();
         }
         public void StopWaves()
         {
@@ -109,31 +131,24 @@ namespace Stirge.Combat
                 return;
             }
 
-            if (m_activeEnemies.Count < m_maxSpawnCount) //there is room for more enemies to spawn
-                if (m_waveDelayCountdown <= 0 && m_spawnRateCountdown <= 0) //check countdowns
-                {
-                    for (int i = 0; i < m_batchSpawn; i++) //attempt to spawn a batch of enemies
-                    {
-                        Enemy newEnemy = m_currentWave.AttemptSpawnEnemy(transform.position);
-                        if (newEnemy)
-                        {
-                            //other spawn functions
-                            m_spawnRateCountdown = m_spawnRate; //reset countdown
-                            m_activeEnemies.Add(newEnemy); //save to list
-                            newEnemy.deathCallback = RemoveEnemyFromActiveList;
-                            m_particles.PlayParticle(m_spawnParticleName, newEnemy.transform);
-                        }
-                        if (m_activeEnemies.Count >= m_maxSpawnCount && !m_canBatchOverflow) break;
-                    }
+            if (m_activeEnemies.Count == 0 && m_currentWave._outOfSpawns) //there are no enemies to be spawned and there are no more living, move on to the next wave
+            {
+                m_waveIndex++;
+                m_currentWave.Init();
+            }
 
-                    if (m_activeEnemies.Count == 0) //there are no enemies have been spawned and there are no more living, move on to the next wave
-                    {
-                        m_waveIndex++;
-                        m_waveDelayCountdown = m_waveDelay;
-                    }
+            Enemy[] enemyBatch = m_currentWave.AttemptSpawnEnemy(transform.position, this);
+            foreach (var newEnemy in enemyBatch)
+            {
+                if (newEnemy)
+                {
+                    //other spawn functions
+                    m_activeEnemies.Add(newEnemy); //save to list
+                    newEnemy.deathCallback = RemoveEnemyFromActiveList;
+                    m_particles.PlayParticle(m_spawnParticleName, newEnemy.transform);
                 }
-            if (m_waveDelayCountdown > 0) m_waveDelayCountdown -= Time.deltaTime; //wave delay
-            if (m_spawnRateCountdown > 0) m_spawnRateCountdown -= Time.deltaTime; //spawn rate
+            }
+
         }
         private void RemoveEnemyFromActiveList(Enemy deadGuy)
         {
